f72ac7e5075e66543ebe5b5a13ac5421862f4d79
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2020-03-20 16:40:15 +0100

    Now matching study

--- patientMatcher.lua
+++ patientMatcher.lua
@@ -1,28 +1,39 @@
 function OnStoredInstance(instanceId, tags, metadata, origin)
+
   -- Prevent infinite loop
-  if origin['RequestOrigin'] ~= 'Lua' then
+  if origin["RequestOrigin"] ~= "Lua" then
     local patientName = string.lower(tags["PatientName"])
     local PatientBirthDate = string.lower(tags["PatientBirthDate"])
-    local response = HttpGet("http://192.168.0.181:8080/patient/" .. PatientBirthDate .. "/" .. patientName)
+    local StudyDate = string.lower(tags["StudyDate"])
+
+    local response = HttpGet("http://<IP:PORT>/study/" .. PatientBirthDate .. "/" .. patientName .. "/" .. StudyDate)
     print(response)
 
     -- only modify if patient match
-    if response ~= nil then
+    if (response ~= "" and response ~= nil )then
+      local parsedResponse = ParseJson(response)
+
       local replace = {}
-      replace["PatientID"] = response
+      replace["PatientID"] = parsedResponse["patientid"]
+      replace["AccessionNumber"] = parsedResponse["studyid"]
 
       -- modify instance
       local command = {}
-      command['Replace'] = replace
-      command['Force'] = true
-      
-      local modifiedFile = RestApiPost('/instances/' .. instanceId .. '/modify', DumpJson(command, true))
+      command["Replace"] = replace
+      command["Force"] = true
+
+      local modifiedFile = RestApiPost("/instances/" .. instanceId .. "/modify", DumpJson(command, true))
 
       -- Upload the modified instance to the Orthanc database so that
       -- it can be sent by Orthanc to other modalities
-      local modifiedId = ParseJson(RestApiPost('/instances/', modifiedFile)) ['ID']
+      local modifiedId = ParseJson(RestApiPost("/instances/", modifiedFile))["ID"]
+      -- Send the modified instance to another modality
+      RestApiPost("/peers/<PEER_NAME>/store", modifiedId)
 
-      RestApiDelete('/instances/' .. instanceId)
+      -- Delete the original
+      RestApiDelete("/instances/" .. instanceId)
+    else
+      RestApiPost("/peers/<PEER_NAME>/store", instanceId)
     end
   end
 end
