21b1da94f94a2d309a7b470572511e4b6edf593e
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2020-04-09 16:56:51 +0200

    modify studyinstanceUID

--- patientMatcher.lua
+++ patientMatcher.lua
@@ -1,41 +1,39 @@
 function OnStoredInstance(instanceId, tags, metadata, origin)
-
   -- Prevent infinite loop
   if origin["RequestOrigin"] ~= "Lua" then
-    local patientName = string.lower(tags["PatientName"])
+    local stationName = tags["StationName"]
+    local physicianName = tags["ReferringPhysicianName"]
+    if (stationName == "SCANNER" and physicianName ~= "A") then
+      return
+    end
+
+    local PatientName = string.lower(tags["PatientName"])
     local PatientBirthDate = string.lower(tags["PatientBirthDate"])
     local StudyDate = string.lower(tags["StudyDate"])
-    -- Need to modify StudyUID and InstanceUID to prevent colision with original study
-    local newStudyInstanceUID = string.lower(tags["StudyInstanceUID"]) .. ".2"
     local newSeriesInstanceUID = string.lower(tags["SeriesInstanceUID"]) .. ".2"
     local newSOPInstanceUID = string.lower(tags["SOPInstanceUID"]) .. ".2"
 
-    local response = HttpGet("http://<IP:PORT>/study/" .. PatientBirthDate .. "/" .. patientName .. "/" .. StudyDate)
-    print(response)
-
+    local response = HttpGet("http://<IP:PORT>/study/" .. PatientBirthDate .. "/" .. PatientName .. "/" .. StudyDate)
     -- only modify if patient match
-    if (response ~= "" and response ~= nil )then
+    if (response ~= "" and response ~= nil) then
       local parsedResponse = ParseJson(response)
-
       local replace = {}
       replace["PatientID"] = parsedResponse["patientid"]
       replace["AccessionNumber"] = parsedResponse["studyid"]
-      replace["StudyInstanceUID"] = newStudyInstanceUID
+      replace["StudyInstanceUID"] = parsedResponse["studyinstanceuid"]
+      replace["SeriesInstanceUID"] = newSeriesInstanceUID
       replace["SOPInstanceUID"] = newSOPInstanceUID
 
       -- modify instance
       local command = {}
       command["Replace"] = replace
       command["Force"] = true
-
       local modifiedFile = RestApiPost("/instances/" .. instanceId .. "/modify", DumpJson(command, true))
-
       -- Upload the modified instance to the Orthanc database so that
       -- it can be sent by Orthanc to other modalities
       local modifiedId = ParseJson(RestApiPost("/instances/", modifiedFile))["ID"]
       -- Send the modified instance to another modality
       RestApiPost("/peers/<PEER_NAME>/store", modifiedId)
-
       -- Delete the original
       RestApiDelete("/instances/" .. instanceId)
     else
