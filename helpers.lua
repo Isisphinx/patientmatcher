b782fdd34ae1eba2a6e80c264bfd3b5b60736e19
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2023-04-20 22:58:56 +0200

    add other helper function

--- helpers.lua
+++ helpers.lua
@@ -4,3 +4,22 @@ function SendToPeers(id)
   RestApiPost('/peers/<peer>/store', DumpJson(command, true) )
   RestApiPost('/modalities/<modality>/store', DumpJson(command, true))
 end
+
+-- Request Matcher
+function RequestMatcher(Ip, Port, rawPatientName, rawPatientBirthDate, rawStudyDate)
+
+  local PatientName = Normalize(rawPatientName)
+  local PatientBirthDate = Normalize(rawPatientBirthDate)
+  local StudyDate = Normalize(rawStudyDate)
+  SetHttpTimeout(1)
+  local matcherResponse = HttpGet("http://" .. Ip .. ':' .. Port .. "/study/" .. PatientBirthDate .. "/" .. PatientName .. "/" .. StudyDate)
+
+  if (matcherResponse == '' or matcherResponse == nil) then return 404 end
+
+  return ParseJson(matcherResponse)
+end
+
+-- Normalize string
+function Normalize(someString)
+  return string.gsub(string.lower(someString), '%s+', '')
+end
