29779e5eecedcae5945a3f0711dda83f917baac0
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2023-06-08 16:27:28 +0200

    Update helpers.lua

--- helpers.lua
+++ helpers.lua
@@ -1,46 +1,47 @@
 -- helpers.lua
 function SendToPeers(id)
-  local command = {}
-  command['Resources'] = {id}
-  command['Asynchronous'] = true
-  RestApiPost('/peers/<peer>/store', DumpJson(command, true) )
-  RestApiPost('/modalities/<modality>/store', DumpJson(command, true))
+    local command = {}
+    command['Resources'] = { id }
+    command['Asynchronous'] = true
+    RestApiPost('/peers/<peer>/store', DumpJson(command, true))
+    RestApiPost('/modalities/<modality>/store', DumpJson(command, true))
 end
 
 -- Request Matcher
 function RequestMatcher(Ip, Port, rawPatientName, rawPatientBirthDate, rawStudyDate)
+    local PatientName = Normalize(rawPatientName)
+    local PatientBirthDate = Normalize(rawPatientBirthDate)
+    local StudyDate = Normalize(rawStudyDate)
+    SetHttpTimeout(1)
+    local matcherResponse = HttpGet("http://" ..
+    Ip .. ':' .. Port .. "/study/" .. PatientBirthDate .. "/" .. PatientName .. "/" .. StudyDate)
 
-  local PatientName = Normalize(rawPatientName)
-  local PatientBirthDate = Normalize(rawPatientBirthDate)
-  local StudyDate = Normalize(rawStudyDate)
-  SetHttpTimeout(1)
-  local matcherResponse = HttpGet("http://" .. Ip .. ':' .. Port .. "/study/" .. PatientBirthDate .. "/" .. PatientName .. "/" .. StudyDate)
+    if (matcherResponse == '' or matcherResponse == nil) then return 404 end
 
-  if (matcherResponse == '' or matcherResponse == nil) then return 404 end
-
-  return ParseJson(matcherResponse)
+    return ParseJson(matcherResponse)
 end
 
 -- Normalize string
 function Normalize(someString)
-  return string.gsub(string.lower(someString), '%s+', '')
+    return string.gsub(string.lower(someString), '%s+', '')
 end
 
 -- Send matching study to peers
-function SendMatchingStudy(patientOrthancID, targetStudyID)
-  -- Get patient details
-  local patientDetails = ParseJson(RestApiGet('/patients/' .. patientOrthancID))
+function ReturnMatchingStudy(patientOrthancID, targetStudyID)
+    -- Get patient details
+    local patientDetails = ParseJson(RestApiGet('/patients/' .. patientOrthancID))
+    local matchedStudyID
 
-  -- Loop through the studies of the patient
-  for _, studyOrthancID in ipairs(patientDetails['Studies']) do
-    -- Get study details
-    local studyDetails = ParseJson(RestApiGet('/studies/' .. studyOrthancID))
+    -- Loop through the studies of the patient
+    for _, studyOrthancID in ipairs(patientDetails['Studies']) do
+        -- Get study details
+        local studyDetails = ParseJson(RestApiGet('/studies/' .. studyOrthancID))
 
-    -- Check if the current study's StudyID matches the targetStudyID
-    if studyDetails['MainDicomTags']['StudyID'] == targetStudyID then
-      -- Send the matching study to peers
-      SendToPeers(studyOrthancID)
-      break
+        -- Check if the current study's StudyID matches the targetStudyID
+        if studyDetails['MainDicomTags']['StudyID'] == targetStudyID then
+            matchedStudyID = studyOrthancID
+            break
+        end
     end
-  end
+    return studyOrthancID
 end
