9a5b14dc2a1fb6c38d370fc33daa84b5c30865e1
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2023-06-08 16:26:48 +0200

    Update patientMatcher.lua

--- patientMatcher.lua
+++ patientMatcher.lua
@@ -3,55 +3,69 @@ local helpers = require('<path>/helpers')
 local config = require('<path>/config')
 
 function OnStableStudy(studyOrthancID, tags, metadata)
-  local stationName = metadata['StationName']
-  local physicianName = metadata['PhysicianName']
-  if (stationName == 'SCANNER' and physicianName ~= 'A') then return end
+    local stationName = metadata['StationName']
+    local physicianName = metadata['PhysicianName']
+    if (stationName == 'SCANNER' and physicianName ~= 'A') then return end
 
-  -- Return early if study is already rectified using the custom DICOM tag
-  if string.find(tags['StudyDescription'], '*fixed*') then return end
+    -- Return early if study is already rectified using the custom DICOM tag
+    if string.find(tags['StudyDescription'], '*fixed*') then return end
 
-  -- Get port and ip
-  local matcherIp = config.matcherIp
-  local matcherPort = config.matcherPort
+    -- Get port and ip
+    local matcherIp = config.matcherIp
+    local matcherPort = config.matcherPort
 
-  -- Get study details with requestedTags query parameter
-  local requestedTags = 'PatientName,PatientBirthDate,StudyDate,ParentPatient'
-  local studyDetails = ParseJson(RestApiGet('/studies/' .. studyOrthancID))
+    -- Get study details with requestedTags query parameter
+    local requestedTags = 'PatientName,PatientBirthDate,StudyDate,ParentPatient'
+    local studyDetails = ParseJson(RestApiGet('/studies/' .. studyOrthancID))
 
-  -- Get patient name, birth date, and study date directly from the studyDetails
-  local patientName = studyDetails['PatientMainDicomTags']['PatientName']
-  local patientBirthDate = studyDetails['PatientMainDicomTags']['PatientBirthDate']
-  local studyDate = studyDetails['MainDicomTags']['StudyDate']
-  local patientOrthancID = studyDetails['ParentPatient']
+    -- Get patient name, birth date, and study date directly from the studyDetails
+    local patientName = studyDetails['PatientMainDicomTags']['PatientName']
+    local patientBirthDate = studyDetails['PatientMainDicomTags']['PatientBirthDate']
+    local studyDate = studyDetails['MainDicomTags']['StudyDate']
+    local patientOrthancID = studyDetails['ParentPatient']
 
-  local matcherResponse = RequestMatcher(matcherIp, matcherPort, patientName, patientBirthDate, studyDate)
+    local matcherResponse = RequestMatcher(matcherIp, matcherPort, patientName, patientBirthDate, studyDate)
 
-  -- Send Study without modification if not found in database and return early
-  if (matcherResponse == 404) then
-    SendToPeers(studyOrthancID)
-    return
-  end
+    -- Send Study without modification if not found in database and return early
+    if (matcherResponse == 404) then
+        SendToPeers(studyOrthancID)
+        return
+    end
 
-  -- Modify the study and mark it as rectified using the custom DICOM tag
-  local studyReplace = {}
-  studyReplace['StudyID'] = matcherResponse['studyid']
-  studyReplace['StudyInstanceUID'] = matcherResponse['studyinstanceuid']
-  studyReplace['StudyDescription'] = tags['StudyDescription'] .. ' *fixed*'
-  local studyCommand = {}
-  studyCommand['Replace'] = studyReplace
-  studyCommand['Force'] = true
-  studyCommand['KeepSource'] = false
-  local modifiedStudyOrthancID = ParseJson(RestApiPost('/studies/' .. studyOrthancID.. '/modify', DumpJson(studyCommand, true)))['ID']
+    -- Modify the study and mark it as rectified using the custom DICOM tag
+    local studyReplace = {}
+    studyReplace['StudyID'] = matcherResponse['studyid']
+    studyReplace['StudyDescription'] = tags['StudyDescription'] .. ' *fixed*'
+    local studyCommand = {}
+    studyCommand['Replace'] = studyReplace
+    studyCommand['Force'] = true
+    studyCommand['KeepSource'] = false
+    local modifiedStudyOrthancID = ParseJson(RestApiPost('/studies/' .. studyOrthancID .. '/modify',
+        DumpJson(studyCommand, true)))['ID']
 
-  -- Modify patient ID
-  local patientReplace = {}
-  patientReplace['PatientID'] = matcherResponse['patientid']
-  local patientCommand = {}
-  patientCommand['Replace'] = patientReplace
-  patientCommand['Force'] = true
-  patientCommand['KeepSource'] = false
-  local modifiedPatientOrthancID = ParseJson(RestApiPost('/patients/' .. patientOrthancID .. '/modify', DumpJson(patientCommand, true)))['ID']
+    -- Modify patient ID
+    local patientReplace = {}
+    patientReplace['PatientID'] = matcherResponse['patientid']
+    local patientCommand = {}
+    patientCommand['Replace'] = patientReplace
+    patientCommand['Force'] = true
+    patientCommand['KeepSource'] = false
+    local modifiedPatientOrthancID = ParseJson(RestApiPost('/patients/' .. patientOrthancID .. '/modify',
+        DumpJson(patientCommand, true)))['ID']
 
-  -- Send rectified study to peers
-  SendMatchingStudy(modifiedPatientOrthancID, matcherResponse['studyid'])
+    -- Modify the studyuid and send to peers
+
+    local newStudyOrthancId = ReturnMatchingStudy(modifiedPatientOrthancID, matcherResponse['studyid'])
+    
+    local studyUIDReplace = {}
+    studyUIDReplace['StudyInstanceUID'] = matcherResponse['studyinstanceuid']
+    local studyUIDCommand = {}
+    studyUIDCommand['Replace'] = studyUIDReplace
+    studyUIDCommand['Force'] = true
+    studyUIDCommand['KeepSource'] = false
+    local modifiedStudyUIDOrthancID = ParseJson(RestApiPost('/studies/' .. newStudyOrthancId .. '/modify',
+        DumpJson(studyUIDCommand, true)))['ID']
+
+    -- Send rectified study to peers
+    SendToPeers(modifiedStudyUIDOrthancID)
 end
